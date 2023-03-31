f8c15d147f54def5d2a72cd054893051b6dfe977
Eobs <michal.kunkel@gmail.com>
2023-03-31 23:53:40 +0200

    Learnt how to get the response from api's url, deal with json data from this api and how to preserve from bad status codes.

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import requests
 
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status() # Raises an error if response != 200
+data = response.json()["iss_position"]
+
+print(data)
